d700993cd10201831ce17c807cb9ad2a08051f9e
Iwerson <guilhermepdo2012@gmail.com>
2024-04-19 01:19:56 -0300

    creating docker container

--- product_manager/settings.py
+++ product_manager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'product_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lista_produtos',
+        'USER': 'adm',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',  # Ou o endereço IP do seu servidor PostgreSQL
+        'PORT': '5432',        # A porta padrão do PostgreSQL
     }
 }
 
